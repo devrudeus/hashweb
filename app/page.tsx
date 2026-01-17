bdfe4560b85ffabffc801e416b3df69cbc25abb8
rudeuscapital <rudeuscapital@gmail.com>
2026-01-17 16:58:04 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -743,7 +743,7 @@ export default function HashWebLanding() {
             <div className="flex items-center justify-center gap-6">
               {/* pump.fun */}
               <a
-                href="https://pump.fun/coin/Tv8BY5PAn1bLXKov3ycwaBZSjn4qYDhdt86eWv2pump"
+                href="#"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="group"
